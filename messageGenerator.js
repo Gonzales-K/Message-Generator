fd117fa4ab574b3dfb8cb783744ab123ad3b6beb
Krystin <krystin.n.gonzales@gmail.com>
2024-07-26 10:09:01 -0700

    Completed JS functionality, modified html, added styling to output

---
+++ messageGenerator.js
@@ -0,0 +1,23 @@
+const heads = ['turkey', 'hippo', 'giraffe'];
+const bodies = ['zebra', 'cow', 'dog'];
+const tails = ['peacock', 'horse', 'pig'];
+const creatureButton = document.getElementById("creatureButton");
+const output = document.getElementById("output");
+
+const randNumber = function(){
+    return Math.floor(Math.random()*3);
+}
+
+function generateCreature(){
+    const head = heads[randNumber()];
+    const body = bodies[randNumber()];
+    const tail = tails[randNumber()];
+    return `Your creature has the head of a ${head}, body of a ${body}, and tail of a ${tail}.`
+}
+console.log(generateCreature())  //works to here
+
+creatureButton.addEventListener("click", function(){
+    let creature = generateCreature();
+    output.innerHTML = creature;
+    console.log(creature)
+})
